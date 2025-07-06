54995a2c4c2d0b383cf59a815c28ae3ba92b8769
Ryan Sheehan <ryan@untitled.stream>
2025-07-06 14:38:19 -0400

    consistent names

--- env.d.ts
+++ env.d.ts
@@ -1,14 +1,14 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-    readonly SANITY_PROJECT_ID: string;
-    readonly SANITY_DATASET: string;
-    readonly SANITY_API_VERSION: string;
-    readonly SANITY_PREVIEW_TOKEN: string;
-    readonly SANITY_PREVIEW_SECRET: string;
+    readonly SANITY_STUDIO_PROJECT_ID: string;
+    readonly SANITY_STUDIO_DATASET: string;
+    readonly SANITY_STUDIO_API_VERSION: string;
+    readonly SANITY_STUDIO_PREVIEW_TOKEN: string;
+    readonly SANITY_STUDIO_PREVIEW_SECRET: string;
     readonly ASTRO_DEV_TOOLBAR_ENABLED: string;
     // client-exposed
-    readonly PUBLIC_SANITY_PROJECT_ID: string;
+    readonly PUBLIC_SANITY_STUDIO_PROJECT_ID: string;
  }
   
   interface ImportMeta {
--- src/pages/api.preview.ts
+++ src/pages/api.preview.ts
@@ -6,7 +6,7 @@ export const GET: APIRoute = async ({ url, redirect }) => {
   const secret = url.searchParams.get('secret')
   const slug = url.searchParams.get('slug')
 
-  if (secret !== import.meta.env.SANITY_PREVIEW_SECRET) {
+  if (secret !== import.meta.env.SANITY_STUDIO_PREVIEW_SECRET) {
     return new Response('Invalid preview secret', { status: 401 })
   }
 
