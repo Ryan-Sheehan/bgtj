f4fa9eef29b1316932132115f8a9cd38f2e1f00d
Ryan Sheehan <ryan@untitled.stream>
2025-07-06 14:51:41 -0400

    error

--- studio/utils/requireEnv.ts
+++ studio/utils/requireEnv.ts
@@ -1,8 +1,7 @@
 export function requireEnv(name: string): string {
     const value = process.env[name]
     if (typeof value === 'undefined') {
-      console.warn(`[WARN] Missing required env var: ${name}`)
-      return ''
+      throw new Error(`Missing required environment variable: ${name}`)
     }
     return value
   }
